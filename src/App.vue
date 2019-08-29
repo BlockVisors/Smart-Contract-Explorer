fd32655a589535c0d2380e1e1185cee668c46d02
Dameon Green <dameongreen@gmail.com>
2019-08-29 10:45:11 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,303 @@
+<template>
+  <div><Reactive /></div>
+</template>
+
+<script>
+import Reactive from "./components/Reactive.vue";
+
+export default {
+  name: "app",
+  components: {
+    Reactive
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: "Raleway", "Arial", sans-serif;
+  background: #f3f6f7;
+}
+/* containers */
+.container {
+  width: 100%;
+  height: 100vh;
+}
+
+.data-search-container {
+  position: fixed;
+  z-index: 3;
+}
+
+.card-icon {
+  margin-right: 4px;
+}
+
+.filters-container {
+  max-height: calc(100vh - 100px);
+  height: 100%;
+  overflow-y: auto;
+}
+
+.results-container {
+  width: calc(100% - 370px);
+}
+
+.result-list {
+  margin-top: 60px;
+}
+
+.result-list-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.result-list-info {
+  margin: 1rem;
+  justify-content: space-between;
+}
+
+.result-list-pagination {
+  margin: 40px 0 50px;
+}
+
+.results-container .powered-by {
+  display: none;
+}
+
+/* header styles */
+.navbar {
+  background: #182636;
+  left: 0;
+  width: 370px;
+  height: 100vh;
+  position: fixed;
+  z-index: 3;
+}
+
+.title {
+  color: black;
+  background: #23354c;
+  font-size: 2rem;
+  text-align: center;
+}
+
+/* components */
+.avatar {
+  height: 50px;
+}
+
+.btn {
+  cursor: pointer;
+  margin: 5px;
+  padding: 5px;
+  text-align: center;
+}
+
+.card-btn {
+  background: #eff3f6;
+  min-width: 100px;
+}
+
+.card-btn:hover {
+  background: #04d07d;
+  color: white;
+}
+.link {
+  cursor: pointer;
+  color: mediumseagreen;
+  text-decoration: none;
+  font-weight: bold;
+  margin-left: 20px;
+  font-size: 1.2rem;
+}
+
+.topic {
+  background: #06f393;
+  margin: 3px;
+  padding: 4px;
+  color: white;
+  font-weight: bold;
+  cursor: pointer;
+  min-width: 55px;
+}
+
+.topic:hover {
+  background: #04d07d;
+}
+
+.topic.active {
+  background: #04d07d;
+}
+
+.toggle-btn {
+  color: #6a6969;
+  border: 1px solid #6a6969;
+  display: none;
+  max-width: 200px;
+  font-size: 1.3rem;
+  align-self: center;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+.toggle-btn:hover {
+  color: #918f8f;
+  border: 1px solid #918f8f;
+}
+
+.range-label {
+  color: white;
+}
+
+.result-item {
+  padding: 20px 0;
+  margin: 15px;
+  padding: 25px;
+  border-radius: 8px;
+  border: 1px solid #eee;
+  flex-basis: 400px;
+  width: 450px;
+  min-height: 300px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: white;
+}
+
+.result-item:hover {
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+}
+
+/* flex layouts */
+.flex {
+  display: flex;
+}
+
+.column {
+  flex-direction: column;
+}
+
+.row-reverse {
+  flex-direction: row-reverse;
+}
+
+.align-center {
+  align-items: center;
+}
+
+.justify-center {
+  justify-content: center;
+}
+
+.justify-end {
+  justify-content: flex-end;
+}
+
+.wrap {
+  flex-wrap: wrap;
+}
+
+.m10 {
+  margin: 10px;
+}
+
+.m10-0 {
+  margin: 10px 0;
+}
+
+.m1,
+.result-list .m1 {
+  margin: 1rem;
+}
+
+/* media queries */
+@media all and (max-width: 1280px) {
+  .result-item {
+    flex-basis: 350px;
+    max-width: 375px;
+  }
+
+  .result-card-header {
+    flex-direction: column;
+  }
+
+  .result-card-header > div {
+    justify-content: center;
+    margin-top: 5px;
+  }
+}
+
+@media all and (max-width: 1200px) {
+  .title {
+    text-align: left;
+  }
+
+  .result-list {
+    margin-top: 170px;
+  }
+
+  .navbar {
+    width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .navbar.active {
+    height: 100vh;
+  }
+
+  .results-container {
+    width: 100%;
+  }
+
+  .data-search-container {
+    margin-top: 20px;
+    width: calc(100% - 280px);
+    right: 20px;
+  }
+
+  .toggle-btn {
+    display: block;
+  }
+
+  .hidden {
+    display: none;
+  }
+}
+
+@media all and (max-width: 992px) {
+  .app-container {
+    flex-direction: column;
+  }
+
+  .result-list {
+    margin-top: 220px;
+  }
+}
+
+@media all and (max-width: 768px) {
+  .data-search-container {
+    width: 100%;
+    right: auto;
+    padding: 0 30px;
+    margin-top: 65px;
+  }
+
+  .search-input {
+    height: 36px;
+  }
+
+  .title {
+    text-align: center;
+    margin-bottom: 50px;
+  }
+}
+</style>
---
+++ src/components/Reactive.vue
@@ -0,0 +1,199 @@
+<template>
+  <section class="container">
+    <ReactiveBase
+      app="gitxplore-app"
+      credentials="4oaS4Srzi:f6966181-1eb4-443c-8e0e-b7f38e7bc316"
+      type="gitxplore-latest"
+    >
+      <div class="flex row-reverse app-container">
+        <Navigation
+          @setTopics="setTopics"
+          v-bind:currentTopics="currentTopics"
+        />
+        <div class="results-container">
+          <DataSearch
+            componentId="repo"
+            filterLabel="Search"
+            :dataField="[
+              'name',
+              'description',
+              'name.raw',
+              'fullname',
+              'owner',
+              'topics'
+            ]"
+            placeholder="Search Smart Contracts"
+            iconPosition="left"
+            :autosuggest="true"
+            URLParams
+            class="data-search-container results-container"
+            :innerClass="{
+              input: 'search-input'
+            }"
+          />
+          <div class="result-list">
+            <SelectedFilters class="m1" />
+            <ReactiveList
+              componentId="results"
+              dataField="name.raw"
+              :pagination="true"
+              :from="0"
+              :size="6"
+              :innerClass="{
+                list: 'result-list-container',
+                pagination: 'result-list-pagination',
+                resultsInfo: 'result-list-info',
+                poweredBy: 'powered-by'
+              }"
+              :react="{
+                and: ['language', 'topics', 'repo', 'stars', 'forks']
+              }"
+              :sortOptions="[
+                {
+                  label: 'Best Match',
+                  dataField: '_score',
+                  sortBy: 'desc'
+                },
+                {
+                  label: 'Most Stars',
+                  dataField: 'stars',
+                  sortBy: 'desc'
+                },
+                {
+                  label: 'Fewest Stars',
+                  dataField: 'stars',
+                  sortBy: 'asc'
+                },
+                {
+                  label: 'Most Forks',
+                  dataField: 'forks',
+                  sortBy: 'desc'
+                },
+                {
+                  label: 'Fewest Forks',
+                  dataField: 'forks',
+                  sortBy: 'asc'
+                },
+                {
+                  label: 'A to Z',
+                  dataField: 'owner.raw',
+                  sortBy: 'asc'
+                },
+                {
+                  label: 'Z to A',
+                  dataField: 'owner.raw',
+                  sortBy: 'desc'
+                },
+                {
+                  label: 'Recently Updated',
+                  dataField: 'pushed',
+                  sortBy: 'desc'
+                },
+                {
+                  label: 'Least Recently Updated',
+                  dataField: 'pushed',
+                  sortBy: 'asc'
+                }
+              ]"
+            >
+              <div slot="onResultStats" slot-scope="{ total, time }">
+                <div class="flex justify-end">
+                  <h5>{{ total }} Results Found In {{ time }} Milliseconds</h5>
+                </div>
+              </div>
+              <div slot="renderData" slot-scope="{ item }">
+                <div key="{{item.name}}" class="result-item">
+                  <div class="flex align-center result-card-header">
+                    <img class="avatar" :src="item.avatar" alt="User avatar" />
+                    <a
+                      class="link"
+                      :href="item.url"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <div class="flex wrap">
+                        <div>{{ item.owner }}/</div>
+                        <div>{{ item.name }}</div>
+                      </div>
+                    </a>
+                  </div>
+                  <div class="m10-0">{{ item.description }}</div>
+                  <div class="flex wrap">
+                    <div
+                      v-for="(tag, index) in item.topics.slice(0, 7)"
+                      :key="index"
+                      v-bind:class="[
+                        currentTopics.includes(tag) ? 'active' : '',
+                        'topic'
+                      ]"
+                      @click="toggleTopic(tag);"
+                    >
+                      #{{ tag }}
+                    </div>
+                  </div>
+                  <div class="flex">
+                    <div>
+                      <div class="btn card-btn">
+                        <font-awesome-icon icon="star" />
+                        {{ item.stars }}
+                      </div>
+                    </div>
+                    <div>
+                      <div class="btn card-btn">
+                        <font-awesome-icon icon="code-branch" />
+                        {{ item.forks }}
+                      </div>
+                    </div>
+                    <div>
+                      <div class="btn card-btn">
+                        <font-awesome-icon icon="eye" />
+                        {{ item.watchers }}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </ReactiveList>
+          </div>
+        </div>
+      </div>
+    </ReactiveBase>
+  </section>
+</template>
+
+<script>
+import Navigation from "./Header.vue";
+
+export default {
+  name: "Reactive",
+  components: {
+    Navigation
+  },
+  data() {
+    return {
+      currentTopics: []
+    };
+  },
+
+  methods: {
+    setTopics(currentTopics) {
+      this.currentTopics = currentTopics || [];
+    },
+    toggleTopic(topic) {
+      const Topics = this.currentTopics;
+      const nextState = Topics.includes(topic)
+        ? Topics.filter(item => item !== topic)
+        : Topics.concat(topic);
+      this.currentTopics = nextState;
+    }
+  }
+};
+</script>
+
+<style>
+.search-input {
+  border: 1px solid #fff !important;
+  background-color: #ffffff !important;
+  height: 60px !important;
+}
+</style>
